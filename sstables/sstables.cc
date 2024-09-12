f24970209fa52feed38c352a965bebee6ecf1cec
Laszlo Ersek <laszlo.ersek@scylladb.com>
2024-09-12 19:05:19 +0200

    sstables: add bytes_on_disk (per-shard) metric
    Introduce the "bytes_on_disk" per-shard stat (gauge), for tracking the
    current disk usage summed over all sstables of the shard.
    
    Upon scylla startup, I have identified the following four sstable method
    call chains:
    
    - discovering an existent sstable (potentially deleting it in the end):
    
      sstable ctor -> load -> close_files -> [unlink]
    
    - creating a new sstable (potentially deleting it in the end):
    
      sstable ctor -> open_sstable -> seal_sstable -> close_files -> [unlink]
    
    On the sstable "read path", increase the stat in load() and decrease it in
    unlink(). On the "write path", increase the stat in seal_sstable() and
    decrease it in unlink().
    
    Define the current disk usage of an sstable as the sum of its recognized
    components' sizes. For that, "_recognized_components" is populated
    
    - on the read path
    
      - in the 1st load variant via: load -> load_metadata -> read_toc,
    
      - in the 2nd load variant via: load -> read_toc,
    
    - on the write path via: open_sstable -> generate_toc.
    
    I can't use the existent bytes_on_disk() function for this purpose.
    Namely, since at least commit 2dbae856f84e ("sstable: Piggyback on sstable
    parser and writer to provide bytes_on_disk", 2023-04-27), bytes_on_disk()
    insists on "_data_file_size" and "_index_file_size" having been set (to
    nonzero values). However, at least "_data_file_size" may not be set when
    we call bytes_on_disk() from seal_sstable(), for example.
    
    Instead, stat each recognized component afresh whenever the sst's disk
    footprint is requested for updating the new metric.
    
    The loop in the new bytes_on_disk_uncached() function is similar to the
    loop removed in commit 2dbae856f84e. One difference is that the new one is
    not specific to "filesystem_storage", i.e., the reactor's file_stat()
    function. Instead, open each component using the sstable's appropriate
    storage backend, and then call the storage-specific file::stat() member
    for fetching the size. For S3, this appears to be satisfied by
    s3::client::readable_file::stat().
    
    Resolves #18191.
    
    Signed-off-by: Laszlo Ersek <laszlo.ersek@scylladb.com>

--- sstables/sstables.cc
+++ sstables/sstables.cc
@@ -1559,6 +1559,21 @@ future<> sstable::reload_reclaimed_components() {
     sstlog.info("Reloaded bloom filter of {}", get_filename());
 }
 
+future<uint64_t> sstable::bytes_on_disk_uncached() const noexcept {
+    uint64_t bytes = 0;
+
+    co_await coroutine::parallel_for_each(_recognized_components, [this, &bytes] (const component_type& type) -> future<> {
+        try {
+            file f = co_await _storage->open_component(*this, type, open_flags::ro, file_open_options{}, false);
+            struct stat st = co_await f.stat().finally([&f] { return f.close(); });
+            bytes += st.st_size;
+        } catch (...) {
+            sstlog.warn("failed to get size of {} for disk usage metric: {}", filename(type), std::current_exception());
+        }
+    });
+    co_return bytes;
+}
+
 future<> sstable::load_metadata(sstable_open_config cfg, bool validate) noexcept {
     co_await read_toc();
     // read scylla-meta after toc. Might need it to parse
@@ -1588,6 +1603,7 @@ future<> sstable::load(const dht::sharder& sharder, sstable_open_config cfg) noe
                 std::vector<unsigned>{this_shard_id()} : compute_shards_for_this_sstable(sharder);
     }
     co_await open_data(cfg);
+    _stats.on_seal_or_load(co_await bytes_on_disk_uncached());
 }
 
 future<> sstable::load(sstables::foreign_sstable_open_info info) noexcept {
@@ -1603,6 +1619,9 @@ future<> sstable::load(sstables::foreign_sstable_open_info info) noexcept {
         validate_partitioner();
         return update_info_for_opened_data();
     }).then([this] {
+        return bytes_on_disk_uncached();
+    }).then([this] (uint64_t bytes) {
+        _stats.on_seal_or_load(bytes);
         _total_reclaimable_memory.reset();
         _manager.increment_total_reclaimable_memory_and_maybe_reclaim(this);
     });
@@ -1904,6 +1923,7 @@ bool sstable::may_contain_rows(const query::clustering_row_ranges& ranges) const
 future<> sstable::seal_sstable(bool backup)
 {
     co_await _storage->seal(*this);
+    _stats.on_seal_or_load(co_await bytes_on_disk_uncached());
     if (_marked_for_deletion == mark_for_deletion::implicit) {
         _marked_for_deletion = mark_for_deletion::none;
     }
@@ -3055,6 +3075,7 @@ future<>
 sstable::unlink(storage::sync_dir sync) noexcept {
     _on_delete(*this);
 
+    uint64_t bytes = co_await bytes_on_disk_uncached();
     auto remove_fut = _storage->wipe(*this, sync);
 
     try {
@@ -3067,7 +3088,7 @@ sstable::unlink(storage::sync_dir sync) noexcept {
     }
 
     co_await std::move(remove_fut);
-    _stats.on_delete();
+    _stats.on_delete(bytes);
     _manager.on_unlink(this);
 }
 
@@ -3190,6 +3211,9 @@ future<> init_metrics() {
 
         sm::make_counter("total_deleted", [] { return sstables_stats::get_shard_stats().deleted; },
             sm::description("Counter of deleted sstables")),
+
+        sm::make_gauge("bytes_on_disk", [] { return sstables_stats::get_shard_stats().bytes_on_disk; },
+            sm::description("Bytes currently used on disk by sstables")),
 
         sm::make_gauge("bloom_filter_memory_size", [] { return utils::filter::bloom_filter::get_shard_stats().memory_size; },
             sm::description("Bloom filter memory usage in bytes.")),
